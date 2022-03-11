df9c8d5af4a6ef3a55df4a182f303b6ac2eee6c8
muba-pro <muba.change@gmail.com>
2022-03-11 23:14:39 +0000

    Print base 16 in lower case

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char hex;
+
+	for (hex = 48; hex <= 57; hex++)
+	{
+		putchar(hex);
+	}
+	for (hex = 'a'; hex <= 'f'; hex++)
+	{
+		putchar(hex);
+	}
+
+	putchar('\n');
+	return (0);
+}
