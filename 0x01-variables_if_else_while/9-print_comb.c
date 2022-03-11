71914839527b72afb4f6b08ad03264a6ad259956
muba-pro <muba.change@gmail.com>
2022-03-11 23:28:07 +0000

    Display single digit numbers followed by comma and space without char type

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 48;
+
+	while (num < 58)
+	{
+		putchar(num);
+		putchar(',');
+		putchar(' ');
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
